9a33236ff4a59a6d17c09d6508a95e56b7959488
danielmishan85 <danielmishan85@gmail.com>
2023-06-22 20:48:16 +0300

    finish app

--- app/src/main/java/com/example/daniellamishan/model/Model.java
+++ app/src/main/java/com/example/daniellamishan/model/Model.java
@@ -17,8 +17,10 @@ public class Model {
 
     private Model() {
         projectList = new ArrayList<>();
+        projectList.add(new Project("android.resource://" + "com.example.daniellamishan" + "/" + R.raw.easyrest, "Java, Android, Fragments, Nav Graph, Parallel programming with threads, Access to Restful API, MongoDB", "https://github.com/danielmishan85/EasyRest-android"));
         projectList.add(new Project("android.resource://" + "com.example.daniellamishan" + "/" + R.raw.recforrest, "Java, Android, Fragments, Nav Graph, FireBase, Parallel programming with threads, weather API", "https://github.com/danielmishan85/RecForRest"));
         projectList.add(new Project("android.resource://" + "com.example.daniellamishan" + "/" + R.raw.foodorderapp, "Java, Android, Design UI", "https://github.com/danielmishan85/Food-Order-App"));
+        projectList.add(new Project("android.resource://" + "com.example.daniellamishan" + "/" + R.raw.clothingstore, "ReactJS, Bootstrap, NodeJS, ExpressJS, MongoDB, Firbase authantication, WebSocket", "https://github.com/danielmishan85/Web-App-Clothingstore"));
         projectList.add(new Project("android.resource://" + "com.example.daniellamishan" + "/" + R.raw.tiktaktoe, "Java, Android", "https://github.com/danielmishan85/TicTacToe"));
         projectList.add(new Project("android.resource://" + "com.example.daniellamishan" + "/" + R.raw.studentslist, "Java, Android", "https://github.com/danielmishan85/StudentsListApp"));
         projectList.add(new Project("android.resource://" + "com.example.daniellamishan" + "/" + R.raw.project1, "NodeJs, HTML", "https://github.com/danielmishan85/Node-farm"));
